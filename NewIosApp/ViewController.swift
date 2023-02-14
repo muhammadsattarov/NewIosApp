c3988a5ba325baa2f9714a72e29ab9bbe7c91f1b
sattarovmuhammad98@gmail.com <sattarovmuhammad98@gmail.com>
2023-02-14 11:26:11 +0500

    Initial Commit

---
+++ NewIosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewIosApp
+//
+//  Created by user on 14/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
